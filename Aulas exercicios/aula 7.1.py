4c6f5fa2ba5cf484d3d214ba6b33050db146813e
Jose Paulo <jhosew@gmail.com>
2021-07-04 12:08:06 -0300

    ok

---
+++ Aulas exercicios/aula 7.1.py
@@ -0,0 +1,9 @@
+n = int(input('Digite um valor: '))
+#d = n * 2
+#t = n * 3
+#r = n ** (1/2)
+print('O dobro de {} vale {}.'.format(n, (n*2)))
+#print('O tiplo de {} vale {}. \nA raiz quadrada de {} e igual a {:.2f}.'.format(n, (n*3), n, (n**(1/2))))
+print ('O triplo de {} vale {}. \nA raiz quadrada de {} e igual a {:.2f}.'.format(n, (n*2), n, pow(n, (1/2))))
+
+
---
+++ Aulas exercicios/aula 7.2.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digite sua primeira nota: '))
+n2 = float(input('Digite sua segunda nota: '))
+r = (n1 + n2) / 2
+print('O Resultado da sua primeira nota: {:.1f} com a sua segunda nota: {:.1f}, deu essa Media: {:.1f}'.format(n1, n2, r))
