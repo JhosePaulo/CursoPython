250fdb870e028b2ef7879d0b7f53cee6777a0d52
Jose Paulo <jhosew@gmail.com>
2021-07-10 14:40:18 -0300

    ok

--- Aulas exercicios/aula 7.10.py
+++ Aulas exercicios/aula 7.10.py
@@ -4,3 +4,4 @@ valor1 = (dias * 60)
 valor2 = (km * 0.15)
 
 print('Voce tera que pagar pelo dias alugado: R$ {:.2f} e o valor pecorrido R$ {:.2f} o total fica R$ {:.2f}'.format(valor1, valor2, (valor1 + valor2)))
+
